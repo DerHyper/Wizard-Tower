6d69ccdbcb300ee26fe5b50329d2a01eb0f11d43
DerHyper <max.richter@hhu.de>
2023-04-15 21:23:54 +0200

    Add Controller to Input Manager

--- Gun-Wizard/Assets/_Scripts/Utilities/InputUtilities.cs
+++ Gun-Wizard/Assets/_Scripts/Utilities/InputUtilities.cs
@@ -7,6 +7,16 @@ public static class InputUtilities
     // Retruns true if a fire button or joystick is pushed in any direction
     public static bool fireButtonPushed()
     {
-        return Input.GetAxisRaw("Horizontal Shoot") != 0 || Input.GetAxisRaw("Horizontal Shoot") != 0;
+        return ControllerFireButtonPushed() || KeyboardFireButtonPushed();
+    }
+
+    private static bool ControllerFireButtonPushed()
+    {
+        return Input.GetAxis("Horizontal Shoot Joystick") != 0 || Input.GetAxis("Vertical Shoot Joystick") != 0;
+    }
+
+    private static bool KeyboardFireButtonPushed()
+    {
+        return Input.GetAxis("Horizontal Shoot") != 0 || Input.GetAxis("Vertical Shoot") != 0;
     }
 }
