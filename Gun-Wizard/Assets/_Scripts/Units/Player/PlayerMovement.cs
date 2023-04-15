6fbff0d588899c1adc335b9badf54ea97f5be7e4
Stephi <stephanie.otto@hhu.de>
2023-04-15 20:20:06 +0200

    Add Player Movement Script

---
+++ Gun-Wizard/Assets/_Scripts/Units/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    //acts as the  motor to move the player
+    public Rigidbody2D rigidbody;
+
+    //stores Player movement
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //value between -1 to 1
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //moves the rigid body to a new position and collides if sth is in the way
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
