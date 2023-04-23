bccaed40e9d751b9a8606c4c232dec059e12f74c
Stephi <stephanie.otto@hhu.de>
2023-04-23 17:19:23 +0200

    add text to display amount of coins add TextMeshPro

--- Gun-Wizard/Assets/_Scripts/ItemCollector.cs
+++ Gun-Wizard/Assets/_Scripts/ItemCollector.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI coinText;
+    private int coins = 0;
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.CompareTag("Coin"))
         {
             Destroy(collider.gameObject);
+            coins++;
+            coinText.text = "Coins: " + coins;
         }
+    }
+
+    public int getCoins()
+    {
+        return coins;
     }
     
 }
