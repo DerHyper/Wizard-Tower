31a7641a103d8211c7b280c1c29d425a287ca490
Stephi <stephanie.otto@hhu.de>
2023-05-10 20:16:50 +0200

    Add Generator for the new Algorithm rooms can no be generated with the Binary Space Partitioning Algortihm

--- Gun-Wizard/Assets/_Scripts/Utilities/ProceduralGenerationAlgorithms.cs
+++ Gun-Wizard/Assets/_Scripts/Utilities/ProceduralGenerationAlgorithms.cs
@@ -86,7 +86,7 @@ public static class ProceduralGenerationAlgorithms
     private static void SplitVertically(int minWidth, Queue<BoundsInt> roomsQueue, BoundsInt room)
     {
         var xSplit = Random.Range(1, room.size.x); //it has a one point spacing on the left and on the right
-        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(xSplit, room.min.y, room.min.z));
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(xSplit, room.size.y, room.size.z));
         BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x + xSplit, room.min.y, room.min.z),
             new Vector3Int(room.size.x - xSplit, room.size.y, room.size.z));
         roomsQueue.Enqueue(room1);
