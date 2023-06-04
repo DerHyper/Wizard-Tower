d05b63e70bad2a6f424abfc1b194045c0dd35efb
DerHyper <max.richter@hhu.de>
2023-06-04 15:50:51 +0200

    Refactor Finder

--- Gun-Wizard/Assets/_Scripts/Utilities/Finder.cs
+++ Gun-Wizard/Assets/_Scripts/Utilities/Finder.cs
@@ -8,42 +8,47 @@ public static class Finder
     public static Logger FindLogger()
     {
         Logger logger = GameObject.FindObjectOfType<Logger>();;
-        if (logger != null)
+        if (logger == null)
         {
-            return logger;
+            GameObject go = Object.Instantiate(new GameObject());
+            return go.AddComponent<Logger>();
         }
-        GameObject go = Object.Instantiate(new GameObject());
-        return go.AddComponent<Logger>();
+        return logger;
     }
 
     // Returns the current Gun from the Player. if no gun is equiped return a standart-Gun
     public static RangedWeaponDisplay FindPlayerGun()
     {
-
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null)
+        GameObject player = FindPlayer();
+        RangedWeaponDisplay rangedWeaponDisplay = player.GetComponentInChildren<RangedWeaponDisplay>();
+        if (rangedWeaponDisplay == null)
         {
-            RangedWeaponDisplay rangedWeaponDisplay = player.GetComponentInChildren<RangedWeaponDisplay>();
-            if (rangedWeaponDisplay != null)
-            {
-                return rangedWeaponDisplay;
-            }
             Debug.Log("Could not find GunDisplay");
-            throw new MissingComponentException();
+            throw new MissingComponentException(); 
         }
-        Debug.Log("Could not find Player");
-        throw new MissingComponentException();
+        return rangedWeaponDisplay;
     }
 
     // Returns the current Inputmanager or creates a new one.
     public static InputManager FindInputManager()
     {
         InputManager inputManager = GameObject.FindObjectOfType<InputManager>();;
-        if (inputManager != null)
+        if (inputManager == null)
         {
-            return inputManager;
+            GameObject go = Object.Instantiate(new GameObject());
+            return go.AddComponent<InputManager>();
         }
-        GameObject go = Object.Instantiate(new GameObject());
-        return go.AddComponent<InputManager>();
+        return inputManager;
+    }
+
+    public static GameObject FindPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            Debug.Log("Could not find Player");
+            throw new MissingComponentException();
+        }
+        return player;
     }
 }
